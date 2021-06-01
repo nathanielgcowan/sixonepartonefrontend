d9b55f7046ff8d289b02b0f318da64bb50838fb4
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-01 10:34:43 -0500

    create the skill

--- frontend/src/skill.js
+++ frontend/src/skill.js
@@ -4,4 +4,14 @@ class Skill{
         this.name = skill.name
         this.creature_id = skill.creature_id
     }
+
+    static createSkill(event){
+        event.preventDefault()
+        const li = document.createElement('li')
+        const skillName = event.target.children[0].name
+        const creatureSkills = event.target.parentElement.dataset.id
+        const creatureId = event.target.previousElement.dataset.id
+        Skill.submitSkill(skillName, creaturesSkills, creatureId)
+        event.target.reset()
+    }
 }
